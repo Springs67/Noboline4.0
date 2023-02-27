33cd386fa170de331cc6cd6d0be519f421ddd09f
Spring67#0001 <109059103+Springs67@users.noreply.github.com>
2023-02-27 15:37:03 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 6872265039 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/Noboline4.0/3kdIKfmsdSf/games/bedwarslobby.lua", true))()
 else
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/Noboline4.0/3kdIKfmsdSf/games/bedwars.lua", true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/Noboline4.0/3kdIKfmsdSf/games/bedwars2.lua", true))()
 end
